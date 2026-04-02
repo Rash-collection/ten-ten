ed7bc29a4d5e0112e963e34fbef500f2f3dd550e
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-04-02 17:59:21 +0300

    database managers 'placeholders' right now

---
+++ src/main/java/contractor/names/NamesManager.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package contractor.names;
+
+/**
+ * <p>Manage all database related updates.</p>
+ * @author rash4
+ */
+public class NamesManager {
+}
