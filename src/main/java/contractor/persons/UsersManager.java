ba3d741e372b6d9938982e2ff73c3e5a546a962e
Rash-collection <195858877+Rash-collection@users.noreply.github.com>
2026-04-03 00:51:24 +0300

    got the first database method to insert row AKA sign-in (register) the 'NewPerson' instance, then if successfully inserted, instantiat new instance of 'User'.. but for now Use class is not finished yet, so it just print the whole row info.

--- src/main/java/contractor/persons/UsersManager.java
+++ src/main/java/contractor/persons/UsersManager.java
@@ -5,9 +5,46 @@
 
 package contractor.persons;
 
+import contractor.core.ConnectDB;
+import contractor.names.Full;
+import contractor.names.Nick;
+import java.sql.SQLException;
+import java.time.Instant;
+import java.util.UUID;
+
 /**
  * <p>Manages all database related updates.</p>
  * @author rash4
  */
-public class UsersManager {
+public final class UsersManager {
+    public static void regex(NewPerson person){
+        registerPerson(person.NAME.first().name(), person.NAME.last().name(), person.NICK.name(), person.pass().pass());
+    }
+    public static void registerPerson(String firstName, String lastName, String nickName, String password){
+        ConnectDB.executeQuery(((
+                """
+                INSERT INTO ten_ten.users (first_n, last_n, nick_n, legendary)
+                VALUES ('%s', '%s', '%s', '%s')
+                RETURNING *;
+                """).formatted(firstName, lastName, nickName, Password.incode(password))), rs->{
+            try {
+                rs.next();
+                final int id = rs.getInt("id");
+                final Full fullName = Full.make(rs.getString("first_n"), rs.getString("last_n"));
+                final UUID token = rs.getObject("access_token", UUID.class);
+                final Instant created = rs.getTimestamp("created_at").toInstant();
+                final Password pass = new Password(Password.decode(rs.getString("legendary")));
+                final Nick nick = new Nick(rs.getString("nick_n"));
+                System.out.println(">> " + id + "||" + fullName + "||" + nick +
+                        "||" + token + "||" + created + "||" + pass.pass());
+                // create the user instance -_-
+            } catch (SQLException e) {
+                System.out.println("SQL ERROR:");
+                System.out.println("Message: " + e.getMessage());
+                System.out.println("SQLState: " + e.getSQLState());
+                System.out.println("Error Code: " + e.getErrorCode());
+                return false;
+            }return true;
+        });
+    }
 }
